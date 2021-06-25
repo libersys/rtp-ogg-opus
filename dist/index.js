d6e01eeb65917cf53cc3f39cfd8a1f7e9c81e4a8
Dioris Moreno <dmoreno@libersys.io>
2021-06-25 11:31:29 -0500

    packages upgraded | using yarn

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,16 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.RtpOpusToPcm = exports.RtpEncoder = exports.RtpDecoder = exports.RtpToOggOpus = exports.OggOpusToRtp = void 0;
 var OggOpusToRtp_1 = require("./OggOpusToRtp");
-Object.defineProperty(exports, "OggOpusToRtp", { enumerable: true, get: function () { return OggOpusToRtp_1.default; } });
+Object.defineProperty(exports, "OggOpusToRtp", { enumerable: true, get: function () { return __importDefault(OggOpusToRtp_1).default; } });
 var RtpToOggOpus_1 = require("./RtpToOggOpus");
-Object.defineProperty(exports, "RtpToOggOpus", { enumerable: true, get: function () { return RtpToOggOpus_1.default; } });
+Object.defineProperty(exports, "RtpToOggOpus", { enumerable: true, get: function () { return __importDefault(RtpToOggOpus_1).default; } });
 var RtpDecoder_1 = require("./RtpDecoder");
-Object.defineProperty(exports, "RtpDecoder", { enumerable: true, get: function () { return RtpDecoder_1.default; } });
+Object.defineProperty(exports, "RtpDecoder", { enumerable: true, get: function () { return __importDefault(RtpDecoder_1).default; } });
 var RtpEncoder_1 = require("./RtpEncoder");
-Object.defineProperty(exports, "RtpEncoder", { enumerable: true, get: function () { return RtpEncoder_1.default; } });
+Object.defineProperty(exports, "RtpEncoder", { enumerable: true, get: function () { return __importDefault(RtpEncoder_1).default; } });
 var RtpOpusToPcm_1 = require("./RtpOpusToPcm");
-Object.defineProperty(exports, "RtpOpusToPcm", { enumerable: true, get: function () { return RtpOpusToPcm_1.default; } });
+Object.defineProperty(exports, "RtpOpusToPcm", { enumerable: true, get: function () { return __importDefault(RtpOpusToPcm_1).default; } });
